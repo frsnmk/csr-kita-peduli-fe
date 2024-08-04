2f4ab7b3c0ebb306bdd1d554581b56f1bbe74389
frsnmk <razedman@gmail.com>
2024-08-04 19:51:45 +0700

    Payment detail page

--- src/app/programs/[id]/page.tsx
+++ src/app/programs/[id]/page.tsx
@@ -2,6 +2,7 @@ import { AvatarListView } from '@/app/ui/avatar-list-view';
 import ArrowBackIconButton from '@/app/ui/icon/arrow-back';
 import PrayerList from '@/app/ui/prayer-list-view';
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 
@@ -68,6 +69,24 @@ export default async function Page({ params }: { params: { id: string } }) {
          <PrayerList />
          <PrayerList />
          <PrayerList />
+        </div>
+        <div className="fixed bottom-0 w-full bg-white shadow-lg max-w-[480px] mx-auto left-0 right-0">
+            <div className="flex justify-around items-center py-3">
+                <button className="flex justify-center space-x-4 w-[40%] border border-green-700 py-2 rounded-lg text-green-700">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#15803d">
+                        <path d="M680-80q-50 0-85-35t-35-85q0-6 3-28L282-392q-16 15-37 23.5t-45 8.5q-50 0-85-35t-35-85q0-50 35-85t85-35q24 0 45 8.5t37 23.5l281-164q-2-7-2.5-13.5T560-760q0-50 35-85t85-35q50 0 85 35t35 85q0 50-35 85t-85 35q-24 0-45-8.5T598-672L317-508q2 7 2.5 13.5t.5 14.5q0 8-.5 14.5T317-452l281 164q16-15 37-23.5t45-8.5q50 0 85 35t35 85q0 50-35 85t-85 35Z"/>
+                    </svg>
+                    <span>Share</span>
+                </button>
+                <Link href="1/donations" className="bg-green-700 flex space-x-4 w-1/2 py-2 rounded-lg text-white font-bold justify-center">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#ffffff">
+                        <path d="M280-159v-361h64q7 0 14 1.5t14 3.5l277 103q14 5 22.5 18t8.5 27q0 21-14.5 34T632-320H527q-5 0-7.5-.5T513-323l-64-25-13 39 77 27q2 1 6 1.5t7 .5h274q32 0 56 23t24 57L561-80l-281-79ZM40-80v-440h160v440H40Zm600-360L474-602q-31-30-52.5-66.5T400-748q0-55 38.5-93.5T532-880q32 0 60 13.5t48 36.5q20-23 48-36.5t60-13.5q55 0 93.5 38.5T880-748q0 43-21 79.5T807-602L640-440Z"/>
+                    </svg>
+                    <span>
+                        Donasi
+                    </span>
+                </Link>
+            </div>
         </div>
     </div>
   )
