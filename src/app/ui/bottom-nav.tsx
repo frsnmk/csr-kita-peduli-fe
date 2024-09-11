15a782e095d33b1c33ce71f9a1730a44646ab7d3
frsnmk <razedman@gmail.com>
2024-09-11 19:06:13 +0700

    Adding Company Menu

--- src/app/ui/bottom-nav.tsx
+++ src/app/ui/bottom-nav.tsx
@@ -8,6 +8,7 @@ import AccountCircleIcon from "./icon/account-circle";
 import Link from "next/link";
 import clsx from "clsx";
 import { usePathname } from "next/navigation";
+import InfoIcon from "./icon/info";
 
 const BottomNav = () => {
   const pathname = usePathname();
@@ -28,17 +29,23 @@ const BottomNav = () => {
           </div>
           <span className={clsx("text-xs", { 'text-green-700': pathname === '/programs', 'text-gray-700': pathname !== '/programs' })}>Program</span>
         </Link>
+        <Link href="/profiles" className="flex flex-col items-center justify-center" passHref>
+          <div className={clsx("px-4 py-1 rounded-lg", { 'bg-green-200': pathname === '/profiles' })}>
+            <AccountCircleIcon className={clsx("h-6 w-6", { 'text-green-700': pathname === '/profiles', 'text-gray-700': pathname !== '/profiles' })} />
+          </div>
+          <span className={clsx("text-xs", { 'text-green-700': pathname === '/profiles', 'text-gray-700': pathname !== '/profiles' })}>Akun</span>
+        </Link>
         <Link href="/histories" className="flex flex-col items-center justify-center" passHref>
           <div className={clsx("px-4 py-1 rounded-lg", { 'bg-green-200': pathname === '/histories' })}>
             <RestoreIcon className={clsx("h-6 w-6", { 'text-green-700': pathname === '/histories', 'text-gray-700': pathname !== '/histories' })} />
           </div>
           <span className={clsx("text-xs", { 'text-green-700': pathname === '/histories', 'text-gray-700': pathname !== '/histories' })}>Riwayat</span>
         </Link>
-        <Link href="/profiles" className="flex flex-col items-center justify-center" passHref>
-          <div className={clsx("px-4 py-1 rounded-lg", { 'bg-green-200': pathname === '/profiles' })}>
-            <AccountCircleIcon className={clsx("h-6 w-6", { 'text-green-700': pathname === '/profiles', 'text-gray-700': pathname !== '/profiles' })} />
+        <Link href="https://kitapeduli.id" className="flex flex-col items-center justify-center" passHref>
+          <div className={"px-4 py-1 rounded-lg"}>
+            <InfoIcon className={"h-6 w-6text-gray-700"} />
           </div>
-          <span className={clsx("text-xs", { 'text-green-700': pathname === '/profiles', 'text-gray-700': pathname !== '/profiles' })}>Akun</span>
+          <span className={"text-xs text-gray-700"}>Company</span>
         </Link>
       </div>
     </div>
